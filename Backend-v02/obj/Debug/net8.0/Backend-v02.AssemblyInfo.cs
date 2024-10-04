93a99d9878b563a1f740514ced69ead548c2493e
SimpleGame <107101075+MikhailVladimirovi4@users.noreply.github.com>
2024-10-04 22:33:22 +0300

    updateStructFolder

--- Backend-v02/obj/Debug/net8.0/Backend-v02.AssemblyInfo.cs
+++ Backend-v02/obj/Debug/net8.0/Backend-v02.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend-v02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce04630f2121ee3262206f707b32ddab21dbf86e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc471e4cca5e525aa6c0dc621cbea740cca99319")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend-v02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend-v02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
