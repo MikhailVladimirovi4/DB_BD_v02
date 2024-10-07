c0ebaaad16fea25d47b231c4ff73d0d8aec46cd6
SimpleGame <107101075+MikhailVladimirovi4@users.noreply.github.com>
2024-10-07 22:41:28 +0300

    Add Doors Repository

--- Backend-v02/DataBaseAccess/Repositories/DoorRepository.cs
+++ Backend-v02/DataBaseAccess/Repositories/DoorRepository.cs
@@ -23,7 +23,56 @@ namespace Backend_v02.DataBaseAccess.Repositories
                 .Select(b => Door.Create(b.Id, b.City, b.Street, b.House, b.Number, b.Ip, b.Escort, b.Device))
                 .ToList();
 
-            return doors;           
+            return doors;
+        }
+
+        public async Task<Guid> Create(Door door)
+        {
+            DoorEntity doorEntity = new DoorEntity
+            {
+                Id = door.Id,
+                City = door.City,
+                Street = door.Street,
+                House = door.House,
+                Number = door.Number,
+                Ip = door.Ip,
+                Escort = door.Escort,
+                Device = door.Device
+            };
+
+            await _context.Doors.AddAsync(doorEntity);
+            await _context.SaveChangesAsync();
+
+            return doorEntity.Id;
+        }
+
+        public async Task<Guid> Update(Guid id, string city, string street, string house, int number, string ip, string escort, string device)
+        {
+            await _context.Doors
+                .Where(b => b.Id == id)
+                .ExecuteUpdateAsync(s => s
+                .SetProperty(b => b.City, b => city)
+                .SetProperty(b => b.Street, b => street)
+                .SetProperty(b => b.House, b => house)
+                .SetProperty(b => b.Number, b => number)
+                .SetProperty(b => b.Ip, b => ip)
+                .SetProperty(b => b.Escort, b => escort)
+                .SetProperty(b => b.Device, b => device));
+
+            await _context.SaveChangesAsync();
+
+            return id;
+        }
+
+        public async Task<Guid> Delete(Guid id)
+        {
+            await _context.Doors
+                .Where (b => b.Id == id)
+                .ExecuteDeleteAsync();
+
+            await _context.SaveChangesAsync();
+
+            return id;
         }
     }
 }
